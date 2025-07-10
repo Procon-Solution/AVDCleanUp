f4be72eed097fd13e7833cc9430c0c991c06d650
Procon-Solution <ie@procon.dk>
2025-07-10 13:51:01 +0200

    Update RemoveApps.ps1

--- RemoveApps.ps1
+++ RemoveApps.ps1
@@ -1,4 +1,4 @@
-$appstoRemove = @("Paint", "OutlookforWindows", "BingSearch", "QuickAssist", "MSTeams", "Edge.GameAssist")
+$appstoRemove = @("OutlookforWindows", "BingSearch", "QuickAssist", "MSTeams", "Edge.GameAssist")
 
 foreach ($app in $appstoremove) {
 	try {
